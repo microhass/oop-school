793143efb611598d95c7a699e91d773659f06588
hassanShakur <hass.shakurhassan@gmail.com>
2023-08-28 13:28:25 +0300

    add trim & capitalize decorators

--- decorators/nameable.rb
+++ decorators/nameable.rb
@@ -4,5 +4,26 @@ class Nameable
     end
 end
 
-n = Nameable.new
-n.correct_name
+class Decorator < Nameable
+    def initialize(nameable)
+        @nameable = nameable
+    end
+
+    def correct_name
+        @nameable.correct_name
+    end
+end
+
+class CapitalizeDecorator < Decorator
+    def correct_name
+        @nameable.correct_name.capitalize
+    end    
+end
+
+class TrimmerDecorator < Decorator
+    def correct_name
+        curr_name = @nameable.correct_name
+        return curr_name if curr_name.length <= 10
+        curr_name.slice(0, 10)
+    end
+end
