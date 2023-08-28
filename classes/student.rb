195efdcdb3db467288e3cfdcb49389d6f9a20750
hassanShakur <hass.shakurhassan@gmail.com>
2023-08-28 15:21:21 +0300

    create rental & book classes

--- classes/student.rb
+++ classes/student.rb
@@ -8,7 +8,7 @@ class Student < Person
 
   attr_reader :classroom
 
-  def set_classroom(classroom)
+  def classroom=(classroom)
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
   end
